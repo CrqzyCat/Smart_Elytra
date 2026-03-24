23a3cfd2b8044d259af2a515e4576a33833ef7e1
crqzycat <crqzycat@gmail.com>
2026-03-24 11:51:54 +0100

    Initial commit

---
+++ src/client/java/me/smart_elytra/smart_elytra/client/Smart_elytraClient.java
@@ -0,0 +1,10 @@
+package me.smart_elytra.smart_elytra.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Smart_elytraClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/smart_elytra/smart_elytra/Smart_elytra.java
@@ -0,0 +1,10 @@
+package me.smart_elytra.smart_elytra;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Smart_elytra implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
